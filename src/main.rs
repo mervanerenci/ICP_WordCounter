3ba31f6452bd9a238e412b54c316183c86e2be2a
mervanerenci <101268022+mervanerenci@users.noreply.github.com>
2024-01-18 10:56:10 +0000

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+struct WordCounter {
+    text: String,
+}
+
+impl WordCounter {
+    fn new(text: &str) -> WordCounter {
+        WordCounter {
+            text: String::from(text),
+        }
+    }
+
+    fn count_words(&self) -> Result<usize, &'static str> {
+        if self.text.trim().is_empty() {
+            Err("Empty string")
+        } else {
+            let words: Vec<&str> = self.text.split_whitespace().collect();
+            Ok(words.len())
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a text:");
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let word_counter = WordCounter::new(&input);
+    let word_count = word_counter.count_words();
+    match word_count {
+        Ok(count) => println!("Word count: {}", count),
+        Err(err) => println!("Error: {}", err),
+    }
 }
